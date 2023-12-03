1410c5143ebfe9f2d21381919a43ba147f22a198
Tropentil <isaacscanlan@outlook.com>
2023-12-03 13:36:20 +0000

    002: Implemented Tune writer

---
+++ TuneConverter.Framework.TuneIO/TuneReader/TuneReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TuneConverter.Framework.TuneIO.TuneReader;
+
+public partial class TuneReader
+{
+
+    private static readonly string _imagePath = "C:/Users/Isaac/source/repos/TuneConverter/TuneConverter.Framework.PageImageIO/InputNotes/";
+
+    public List<List<string>> readFile(String tune)
+    {
+        var lines = ByNewLine().Split(File.ReadAllText(_imagePath + tune)).ToList();
+
+        List<List<string>> output = new();
+        List<string> partlist = new();
+        foreach (var line in lines)
+        {
+            var newLine = ByTab().Replace(line, "");
+            if (newLine.Equals("__"))
+            {
+                output.Add(partlist);
+                partlist = new();
+                continue;
+            }
+            partlist.Add(newLine);
+        }
+
+        return output;
+    }
+
+    [GeneratedRegex(@"\r\n")]
+    private static partial Regex ByNewLine();
+    [GeneratedRegex(@"\t")]
+    private static partial Regex ByTab();
+}
